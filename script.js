df1f588ed52cc963ed41cb6abb5b7da64d556990
SwayKh <khatrisumit712@gmail.com>
2025-05-12 22:18:58 +0530

    Add a temporary working 30 min timer display

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + ": " + seconds;
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+var countTime = 1800;
+var timer = setInterval(function () {
+  if (countTime < 0) {
+    clearInterval(timer);
+    document.getElementById("demo").innerHTML = "00:00";
+    return;
   }
+
+  var minutes = Math.floor(countTime / 60);
+  var seconds = Math.floor(countTime % 60);
+
+  time = minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+
+  document.getElementById("demo").innerHTML = time;
+
+  countTime--;
 }, 1000);
